a52228f164a231940a8690a150cffe5440bdafda
pandapanda6666 <50913panda@gmail.com>
2026-08-06 15:51:51 +0800

    v1.3.10: Fix player.html paths and window.encryptSb3 scope

--- scratch/projects/editor/panda_guard.js
+++ scratch/projects/editor/panda_guard.js
@@ -1,6 +1,44 @@
 (function() {
     console.log("PandaGuard: Initializing universal download interceptor with Blob caching...");
 
+    window.encryptSb3 = async function(originalBlob) {
+        const JSZip = window.JSZip;
+        if (!JSZip) throw new Error("JSZip not found!");
+
+        const originalZip = await JSZip.loadAsync(originalBlob);
+        const newZip = new JSZip();
+        const pandaFolder = newZip.folder("panda_project");
+        
+        for (const f of Object.keys(originalZip.files)) {
+            if (!originalZip.files[f].dir) {
+                const fileData = await originalZip.files[f].async("uint8array");
+                if (f === "project.json") {
+                    pandaFolder.file("panda.json", fileData);
+                } else {
+                    pandaFolder.file(f, fileData);
+                }
+            }
+        }
+        
+        const warningZipBase64 = window.PANDA_WARNING_ZIP_BASE64;
+        if (warningZipBase64) {
+            const warningZip = await JSZip.loadAsync(warningZipBase64, {base64: true});
+            for (const f of Object.keys(warningZip.files)) {
+                if (!warningZip.files[f].dir) {
+                    const fileData = await warningZip.files[f].async("uint8array");
+                    newZip.file(f, fileData);
+                }
+            }
+        }
+
+        return await newZip.generateAsync({
+            type: "blob",
+            mimeType: "application/x.scratch.sb3",
+            compression: "DEFLATE",
+            compressionOptions: { level: 6 }
+        });
+    };
+
     // 1. Intercept URL.createObjectURL to catch the Blob before it gets downloaded/revoked
     const originalCreateObjectURL = window.URL.createObjectURL;
     window.pandaGuardBlobs = new Map();
@@ -81,45 +119,7 @@
             }
         }
 
-        window.encryptSb3 = async function(originalBlob) {
-        const JSZip = window.JSZip;
-        if (!JSZip) throw new Error("JSZip not found!");
-
-        const originalZip = await JSZip.loadAsync(originalBlob);
-        const newZip = new JSZip();
-        const pandaFolder = newZip.folder("panda_project");
-        
-        for (const f of Object.keys(originalZip.files)) {
-            if (!originalZip.files[f].dir) {
-                const fileData = await originalZip.files[f].async("uint8array");
-                if (f === "project.json") {
-                    pandaFolder.file("panda.json", fileData);
-                } else {
-                    pandaFolder.file(f, fileData);
-                }
-            }
-        }
-        
-        const warningZipBase64 = window.PANDA_WARNING_ZIP_BASE64;
-        if (warningZipBase64) {
-            const warningZip = await JSZip.loadAsync(warningZipBase64, {base64: true});
-            for (const f of Object.keys(warningZip.files)) {
-                if (!warningZip.files[f].dir) {
-                    const fileData = await warningZip.files[f].async("uint8array");
-                    newZip.file(f, fileData);
-                }
-            }
-        }
-
-        return await newZip.generateAsync({
-            type: "blob",
-            mimeType: "application/x.scratch.sb3",
-            compression: "DEFLATE",
-            compressionOptions: { level: 6 }
-        });
-    };
-
-    const newBlob = await newZip.generateAsync({
+        const newBlob = await newZip.generateAsync({
             type: "blob",
             mimeType: "application/x.scratch.sb3",
             compression: "DEFLATE",
