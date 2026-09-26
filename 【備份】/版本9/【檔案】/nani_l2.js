deab004d0b984ae2830134391c6ee9529b58a842
pandapanda6666 <50913panda@gmail.com>
2026-09-26 11:07:53 +0800

    Hotfix: resolve ReferenceError caused by temporal dead zone of isSpellLocked

---
+++ 【備份】/版本9/【檔案】/nani_l2.js
@@ -0,0 +1,182 @@
+const naniL2 = [
+  {
+    "word": "postcard",
+    "translation": "明信片",
+    "pos": "n.",
+    "audio": "audio/L2w_postcard.mp3"
+  },
+  {
+    "word": "the USA",
+    "translation": "美國（＝ the U.S.A.）",
+    "pos": "abbr.",
+    "audio": "audio/L2w_the_USA.mp3"
+  },
+  {
+    "word": "year(s) old",
+    "translation": "歲",
+    "pos": "phr.",
+    "audio": "audio/L2w_years_old.mp3"
+  },
+  {
+    "word": "famous",
+    "translation": "有名的",
+    "pos": "adj.",
+    "audio": "audio/L2w_famous.mp3"
+  },
+  {
+    "word": "beautiful",
+    "translation": "漂亮的",
+    "pos": "adj.",
+    "audio": "audio/L2w_beautiful.mp3"
+  },
+  {
+    "word": "all over the world",
+    "translation": "世界各地",
+    "pos": "phr.",
+    "audio": "audio/L2w_all_over_the_world.mp3"
+  },
+  {
+    "word": "hard",
+    "translation": "困難的",
+    "pos": "adj.",
+    "audio": "audio/L2w_hard.mp3"
+  },
+  {
+    "word": "easy",
+    "translation": "簡單的",
+    "pos": "adj.",
+    "audio": "audio/L2w_easy.mp3"
+  },
+  {
+    "word": "late",
+    "translation": "遲的",
+    "pos": "adj.",
+    "audio": "audio/L2w_late.mp3"
+  },
+  {
+    "word": "tomorrow",
+    "translation": "（在）明天",
+    "pos": "adv.",
+    "audio": "audio/L2w_tomorrow.mp3"
+  },
+  {
+    "word": "family",
+    "translation": "家庭",
+    "pos": "n.",
+    "audio": "audio/L2w_family.mp3"
+  },
+  {
+    "word": "farmer",
+    "translation": "農夫",
+    "pos": "n.",
+    "audio": "audio/L2w_farmer.mp3"
+  },
+  {
+    "word": "housewife",
+    "translation": "家庭主婦",
+    "pos": "n.",
+    "audio": "audio/L2w_housewife.mp3"
+  },
+  {
+    "word": "clerk",
+    "translation": "職員",
+    "pos": "n.",
+    "audio": "audio/L2w_clerk.mp3"
+  },
+  {
+    "word": "actress",
+    "translation": "女演員",
+    "pos": "n.",
+    "audio": "audio/L2w_actress.mp3"
+  },
+  {
+    "word": "son",
+    "translation": "兒子",
+    "pos": "n.",
+    "audio": "audio/L2w_son.mp3"
+  },
+  {
+    "word": "daughter",
+    "translation": "女兒",
+    "pos": "n.",
+    "audio": "audio/L2w_daughter.mp3"
+  },
+  {
+    "word": "husband",
+    "translation": "丈夫",
+    "pos": "n.",
+    "audio": "audio/L2w_husband.mp3"
+  },
+  {
+    "word": "wife",
+    "translation": "妻子",
+    "pos": "n.",
+    "audio": "audio/L2w_wife.mp3"
+  },
+  {
+    "word": "junior high school",
+    "translation": "國中",
+    "pos": "n.",
+    "audio": "audio/L2w_junior_high_school.mp3"
+  },
+  {
+    "word": "star",
+    "translation": "明星",
+    "pos": "n.",
+    "audio": "audio/L2w_star.mp3"
+  },
+  {
+    "word": "still",
+    "translation": "還，仍然",
+    "pos": "adv.",
+    "audio": "audio/L2w_still.mp3"
+  },
+  {
+    "word": "cute",
+    "translation": "可愛的",
+    "pos": "adj.",
+    "audio": "audio/L2w_cute.mp3"
+  },
+  {
+    "word": "wonderful",
+    "translation": "絕妙的，極好的",
+    "pos": "adj.",
+    "audio": "audio/L2w_wonderful.mp3"
+  },
+  {
+    "word": "place",
+    "translation": "地方",
+    "pos": "n.",
+    "audio": "audio/L2w_place.mp3"
+  },
+  {
+    "word": "theme park",
+    "translation": "主題樂園",
+    "pos": "n.",
+    "audio": "audio/L2w_theme_park.mp3"
+  },
+  {
+    "word": "founder",
+    "translation": "創辦者",
+    "pos": "n.",
+    "audio": "audio/L2w_founder.mp3"
+  },
+  {
+    "word": "cartoon",
+    "translation": "卡通",
+    "pos": "n.",
+    "audio": "audio/L2w_cartoon.mp3"
+  },
+  {
+    "word": "father",
+    "translation": "創始者",
+    "pos": "n.",
+    "audio": "audio/L2w_father.mp3"
+  },
+  {
+    "word": "have a great time",
+    "translation": "玩得開心",
+    "pos": "phr.",
+    "audio": "audio/L2w_have_a_great_time.mp3"
+  }
+];
